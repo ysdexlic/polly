5107353b9fbd14e6ababae1720261eb82ea39726
ysdexlic <david.arthur.thompson@gmail.com>
2018-06-13 21:14:17 +0100

    using react-native-swiper

--- app/modules/home/scenes/Home/Home.js
+++ app/modules/home/scenes/Home/Home.js
@@ -1,62 +1,56 @@
 import React, { Component } from 'react'
-import { View, Easing, Animated } from 'react-native'
-import { createStackNavigator } from 'react-navigation'
+import { View, Text } from 'react-native'
+import Swiper from 'react-native-swiper'
 
 import ProfileScreen from '../Profile'
 import SearchScreen from '../Search'
 import SettingsScreen from '../Settings'
 
-const transitionConfig = () => {
-    return {
-        transitionSpec: {
-            duration: 750,
-            easing: Easing.out(Easing.poly(4)),
-            timing: Animated.timing,
-            useNativeDriver: true,
-        },
-        screenInterpolator: sceneProps => {
-            console.log(sceneProps)
-            const { layout, position, scene } = sceneProps
 
-            const thisSceneIndex = scene.index
-            const width = layout.initWidth
+export class Home extends Component {
+    constructor() {
+        super()
+        this.state = {
+            x: 0
+        }
+    }
 
-            const translateX = position.interpolate({
-                inputRange: [thisSceneIndex - 1, thisSceneIndex],
-                outputRange: [width, 0],
-            })
+    viewStyle() {
+        return {
+            flex: 1,
+            justifyContent: 'center',
+            alignItems: 'center',
+        }
+    }
 
-            return { transform: [ { translateX } ] }
-        },
+    onScroll = (e) => {
+        this.setState({x: e.nativeEvent.contentOffset.x})
+    }
+
+    render() {
+        return (
+            <View style={this.viewStyle()}>
+                <Swiper
+                    loop={false}
+                    showsPagination={false}
+                    index={1}
+                    bounces={true}
+                    onScroll={this.onScroll}
+                    scrollEventThrottle={16}>
+                    <View style={this.viewStyle()}>
+                        <SettingsScreen />
+                    </View>
+                    <View style={this.viewStyle()}>
+                        <ProfileScreen />
+                    </View>
+                    <View style={this.viewStyle()}>
+                        <SearchScreen />
+                    </View>
+                </Swiper>
+                <Text>{this.state.x}</Text>
+            </View>
+        )
     }
 }
-
-export const Home = createStackNavigator({
-    Settings: { screen: SettingsScreen },
-    Profile: { screen: ProfileScreen },
-    Search: { screen: SearchScreen },
-}, {
-    initialRouteName: 'Profile',
-    // headerMode: 'none',
-    transitionConfig,
-    cardStyle: {
-        shadowColor: 'transparent',
-    }
-})
-
-// export const Home = createBottomTabNavigator({
-//     Profile: ProfileScreen,
-//     Search: SearchScreen,
-// })
-
-// export class Home extends Component {
-//     render() {
-//         return (
-//             <View style={{flex: 1}}>
-//                 <Navigator />
-//             </View>
-//         )
-//     }
-// }
 
 export default Home
