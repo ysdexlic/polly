3de90532bf77851a8e421a4006208a08738417f1
David <david.arthur.thompson@gmail.com>
2018-06-14 20:14:27 +0100

    noice noice noice animations

--- app/modules/home/scenes/Home/Home.js
+++ app/modules/home/scenes/Home/Home.js
@@ -12,12 +12,12 @@ export class Home extends Component {
     constructor() {
         super()
         this.state = {
-            opacity1: 1,
-            opacity2: 1,
-            opacity3: 1
+            linearValue1: new Animated.Value(1),
+            linearValue2: new Animated.Value(1),
+            linearValue3: new Animated.Value(1),
+            translateX: new Animated.Value(0)
         }
         this.width = Dimensions.get('window').width
-        this.translateX = new Animated.Value(375)
     }
 
     viewStyle() {
@@ -28,41 +28,79 @@ export class Home extends Component {
         }
     }
 
-    getOpacity = (index, x) => {
-        // middle point: i * w
-        // lowest point: ((i * w) - w)
-        // highest point: ((i * w) + w)
+    goToIndex = (index) => {
+        this.swiper.scrollView.scrollTo({x: index * this.width})
+    }
 
-        const lowest = (index * this.width) - this.width
-        const highest = (index * this.width) + this.width
-        const opacity = - (x - (lowest/this.width)) * (x - (highest/this.width))
-        return opacity + 0.4
+    getLinearValue = (index, x) => {
+        const value = new Animated.Value(1 - Math.abs(x - index))
+        return value
     }
 
     onScroll = (e) => {
-        const value = - (e.nativeEvent.contentOffset.x - this.width) / 2.5
+        const translateX = - (e.nativeEvent.contentOffset.x - this.width) / 2.5
         const currentIndex = e.nativeEvent.contentOffset.x / this.width
 
-        this.element.setNativeProps({
-            style: {
-                transform: [{
-                    translateX: value
-                }]
-            }
-        })
-
         this.setState({
-            opacity1: this.getOpacity(0, currentIndex),
-            opacity2: this.getOpacity(1, currentIndex),
-            opacity3: this.getOpacity(2, currentIndex),
+            translateX,
+            linearValue1: this.getLinearValue(0, currentIndex),
+            linearValue2: this.getLinearValue(1, currentIndex),
+            linearValue3: this.getLinearValue(2, currentIndex),
         })
     }
 
+    componentDidMount() {
+        // TODO: remove this massive hack to fix onScroll triggering on load
+        setTimeout(() => {
+            this.onScroll({nativeEvent: {contentOffset: {x: 375}}})
+        }, 1)
+    }
 
     render() {
+        // TODO: move animated icon to own component so I don't have to do everything 3 fucking times
+        const color1 = this.state.linearValue1.interpolate({
+            inputRange: [0, 1],
+            outputRange: ['#000', '#FF553F']
+        })
+        const color2 = this.state.linearValue2.interpolate({
+            inputRange: [0, 1],
+            outputRange: ['#000', '#FF553F']
+        })
+        const color3 = this.state.linearValue3.interpolate({
+            inputRange: [0, 1],
+            outputRange: ['#000', '#FF553F']
+        })
+
+        const opacity1 = this.state.linearValue1.interpolate({
+            inputRange: [0, 1],
+            outputRange: [0.6, 1],
+        })
+        const opacity2 = this.state.linearValue2.interpolate({
+            inputRange: [0, 1],
+            outputRange: [0.6, 1],
+        })
+        const opacity3 = this.state.linearValue3.interpolate({
+            inputRange: [0, 1],
+            outputRange: [0.6, 1],
+        })
+
+        const scale1 = this.state.linearValue1.interpolate({
+            inputRange: [0, 1],
+            outputRange: [1, 1.5],
+        })
+        const scale2 = this.state.linearValue2.interpolate({
+            inputRange: [0, 1],
+            outputRange: [1, 1.5],
+        })
+        const scale3 = this.state.linearValue3.interpolate({
+            inputRange: [0, 1],
+            outputRange: [1, 1.5],
+        })
+
         return (
             <View style={this.viewStyle()}>
                 <Swiper
+                    ref={(ref) => this.swiper = ref}
                     loop={false}
                     showsPagination={false}
                     index={1}
@@ -86,12 +124,18 @@ export class Home extends Component {
                         paddingHorizontal: 30,
                         width: '100%',
                         justifyContent: 'space-between',
-                        transform: [{translateX: this.translateX}]
+                        transform: [{translateX: this.state.translateX}]
                     }}
                 >
-                    <Icon name="ios-settings-outline" size={30} style={{color: 'blue', opacity: this.state.opacity1}}/>
-                    <Icon name="ios-home-outline" size={30} style={{color: 'blue', opacity: this.state.opacity2}}/>
-                    <Icon name="ios-search-outline" size={30} style={{color: 'blue', opacity: this.state.opacity3}}/>
+                    <Animated.Text style={{color: color1, opacity: opacity1, transform: [{scale: scale1}]}}>
+                        <Icon name="ios-settings-outline" size={30} onPress={() => this.goToIndex(0)} />
+                    </Animated.Text>
+                    <Animated.Text style={{color: color2, opacity: opacity2, transform: [{scale: scale2}]}}>
+                        <Icon name="ios-home-outline" size={30} onPress={() => this.goToIndex(1)} />
+                    </Animated.Text>
+                    <Animated.Text style={{color: color3, opacity: opacity3, transform: [{scale: scale3}]}}>
+                        <Icon name="ios-search-outline" size={30} onPress={() => this.goToIndex(2)} />
+                    </Animated.Text>
                 </Animated.View>
             </View>
         )
